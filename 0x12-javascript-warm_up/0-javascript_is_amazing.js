b7d3b3d808f019b8d81ec35561bf2a4f33bd0fd6
Nicholus Maingi <mainginicholus@gmail.com>
2023-03-13 07:45:57 -0700

    mandatory tasks 0-14

---
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/**
+script to rn on console
+**/
+const myVar ="JavaScript is amazing";
+
+
+console.log(myVar);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for(i =0; i<args.length; i++){
+console.log(args[i]);
+}
