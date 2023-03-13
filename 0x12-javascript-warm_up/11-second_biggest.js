cce03a0e215add64bc81d7a8691a2eb6c2089707
Nicholus Maingi <mainginicholus@gmail.com>
2023-03-13 08:57:50 -0700

    second biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
-if(process.argv.length <= 3){
- console.log(0);
-}
-else {
-	const numbers = process.argv.sort();
-	console.log(numbers.reverse()[1]);
+if (process.argv.length <= 3) {
+  console.log(0);
+} else {
+  const numbers = process.argv.sort();
+  console.log(numbers.reverse()[1]);
 }
