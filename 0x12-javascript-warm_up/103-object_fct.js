91ac25effd6b47ffdcc42f2913eb359152b205c3
Nicholus Maingi <mainginicholus@gmail.com>
2023-03-13 08:10:06 -0700

    additional advanced tasks

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+//incr function
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.incr = function () {
+  myObject.value++;
+};
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
