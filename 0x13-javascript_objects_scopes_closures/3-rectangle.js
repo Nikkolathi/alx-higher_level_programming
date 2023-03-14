a2858b5d98cbcddf1c619e8dcf41923192552a9d
Nicholus Maingi <mainginicholus@gmail.com>
2023-03-14 08:48:15 -0700

    3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,8 +1,11 @@
 #!/usr/bin/node
 //  class rectangle with a method print()
-module.exports = class Reactangle {
+//
+module.exports = class Rectangle {
   constructor (w, h) {
-    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
   }
 
   print () {
