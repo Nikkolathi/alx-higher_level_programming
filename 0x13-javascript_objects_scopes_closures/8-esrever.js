fc4fd9b0049d1defb0746905326989dbd7205575
Nicholus Maingi <mainginicholus@gmail.com>
2023-03-14 08:08:02 -0700

    8-esrever.js - reverse a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// function to return the reversed version of a list withoutthe inbuilt reverse
+
+exports.esrever = function (list) {
+  const reversedList = [];
+  const last = list.length - 1;
+  for (let i = last; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return (reversedList);
+};
